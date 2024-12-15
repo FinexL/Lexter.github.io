4fdd8af0fc87583e4046a1c6b569dde31544cc11
FinexL <chickencurry972@gmail.com>
2024-12-16 04:16:24 +0800

    Done Homepage

--- Asset/Script/script.js
+++ Asset/Script/script.js
@@ -1,15 +1,21 @@
-    const sidenav = document.getElementById('sidenav');
-    const mainContent = document.getElementById('main');
-    const toggleButton = document.getElementById('toggleSidebar');
+const sideNav = document.getElementById('sidenav');
+const mainContent = document.getElementById('main');
+const toggleButton = document.getElementById('toggleSidebar');
+const isCollapsed = localStorage.getItem('sideNavCollapsed') === 'true';
+
+if (isCollapsed) {
+  sideNav.classList.add('collapsed');
+  mainContent.classList.add('expanded');
+}
+
+toggleButton.addEventListener('click', () => {
+  sidenav.classList.toggle('collapsed');
+  mainContent.classList.toggle('expanded');
+
+  const currentState = sideNav.classList.contains('collapsed');
+  localStorage.setItem('sideNavCollapsed', currentState);
+});
 
 
-    
-    toggleButton.addEventListener('click', () => 
-    {
-      sidenav.classList.toggle('collapsed');
-      mainContent.classList.toggle('expanded');
-    });
 
-    const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
-    const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
     
